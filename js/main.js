7ad746646db55699a72383941a463e45e2ee6b70
Megan Jackson <meganjackson@ufl.edu>
2017-03-31 10:55:51 -0700

    use the paragraphs variable to pass an object to the .css jQuery method altering the style of the paragraphs

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 $( document ).ready(function() {
 	var paragraphs = $('p');
-
+	$('p').css({'font-weight': 'normal', 'font-size': '1em', 'line-height': '1.5em', 'color': '#161616', 'padding-left': '1em'});
 });
